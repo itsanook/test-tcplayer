87dd791e132446d4348a3ff6a52293f2b4356430
ivan <ivanli@tencent.com>
2017-11-08 10:37:24 +0800

    v0.1.13-8 for pretest

--- src/TCPlayer/Constants.php
+++ src/TCPlayer/Constants.php
@@ -15,7 +15,7 @@ class Constants {
       "vod-ima-0.1.4" => "vendor-ext/tcplayer/vod-ima-0.1.4.js",
       "vod-ima-0.1.6" => "vendor-ext/tcplayer/vod-ima-0.1.6-2.js",
       "vod-ima-0.1.10" => "vendor-ext/tcplayer/vod-ima-0.1.10.js",
-      "vod-ima-0.1.13" => "vendor-ext/tcplayer/vod-ima-0.1.13-7.js",
+      "vod-ima-0.1.13" => "vendor-ext/tcplayer/vod-ima-0.1.13-8.js",
       "vod-ima-ibg" => "//p.ibg.wechatapp.com/bossapp_arthur/vod-ima-dev/js/vod-ima.js",
       // "TcPlayer-2.2.0.js" => "//imgcache.qq.com/open/qcloud/video/vcplayer/TcPlayer-2.2.0.js"
     ];
